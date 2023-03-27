51957777975badd76bb4afe6fa3fb4fdcb8152ea
Agustin Baza <agustinbaza10@gmail.com>
2023-03-27 15:36:49 -0300

    Agregue y controle funcionamiento del ejercicio 1

---
+++ js/EJ-1.js
@@ -0,0 +1,15 @@
+// PUNTO 1 
+
+let auto = {
+    color: "Rojo",
+    marca: "Ferrari",
+    modelo: "Portofino",
+
+    encender: ()=> {
+        document.write("Auto encendido");
+    },
+    apagar: ()=> {
+        document.write("El auto se apago");
+    }
+}
+
