5b620cfb9d9dd756724907ccd06bd6611602c014
Agustin Baza <agustinbaza10@gmail.com>
2023-04-03 14:35:09 -0300

    Agregue y controle funcionamiento del ejercicio 8

---
+++ js/EJ-7.js
@@ -0,0 +1,21 @@
+// PUNTO 7
+
+class Contacto{
+    constructor(nombre,telefono){
+    this.nombre = nombre;
+    this.telefono = telefono;
+} 
+}
+
+class Agenda {
+    constructor(contacto,tamaño){
+        this.contacto = contacto;
+        this.tamaño = tamaño;
+    }
+
+    anadirContacto(contacto) {
+        if (this.tamaño <=10) {
+        
+    }
+}
+}
